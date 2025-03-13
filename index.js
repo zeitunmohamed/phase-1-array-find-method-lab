04efe2329a43358fac9534cdadbdb2e96bf4e8ef
zeitunmohamed <zeitunmohamed@gmail.com>
2025-03-13 17:39:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// helpers.js
+
+function superbowlWin(record) {
+    // Find the first record with a win ("W")
+    const win = record.find(season => season.result === "W");
+    
+    // If a win is found, return the year; otherwise, return undefined
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
